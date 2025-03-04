bfcfac38889e477f1da4902f8694f522cba26f22
Ilton Andrew <iltonandrew@gmail.com>
2025-03-04 03:30:44 -0300

    🔧 feat: log VERCEL_URL in getBaseUrl function for debugging purposes

--- src/utils/getBaseUrl.ts
+++ src/utils/getBaseUrl.ts
@@ -1,4 +1,5 @@
 export const getBaseUrl = () => {
+  console.log(process.env.VERCEL_URL);
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   return "http://localhost:3000";
 };
